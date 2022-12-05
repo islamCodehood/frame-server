d52b47f247d2b09b47acd56d868e2f219a4c956f
Islam Sayed <islam.sayed8@gmail.com>
2022-12-05 16:35:39 +0200

    create user routes, model, controllers

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+// import movieRoutes from "./routes/movie.routes";
+import userRoutes from "./routes/user.routes";
+// import storyRoutes from "./routes/story.routes";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json({ limit: "30mb", strict: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+//movie route
+// app.use("/movie", movieRoutes);
+//user route
+app.use("/user", userRoutes);
+// app.use("/story", storyRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+mongoose
+  .connect(process.env.CONNECTION_URL!)
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`server running at port: ${process.env.PORT}`)
+    )
+  )
+  .catch((err) => {
+    console.log(err.message);
+  });
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { signin, signup } from "../controllers/user.controllers"
+
+const router = express.Router();
+
+//refer to controllers for these functions
+router.post("/signin", signin);
+router.post("/signup", signup);
+
+export default router;
