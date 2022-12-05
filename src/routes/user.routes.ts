4fd9baaa70c8725323331daeea3f25dd7a928cec
Islam Sayed <islam.sayed8@gmail.com>
2022-12-05 18:23:53 +0200

    create all-users route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { signin, signup } from '../controllers/user.controllers'
+import { signin, signup, getAllUsers } from '../controllers/user.controllers'
 
 const router = express.Router()
 
 //refer to controllers for these functions
 router.post('/signin', signin)
 router.post('/signup', signup)
+router.get('/all-users', getAllUsers)
 
 export default router
