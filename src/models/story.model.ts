21aa4ab8dec12dd6d96ebe50c82ba2595643741e
Islam Sayed <islam.sayed8@gmail.com>
2022-12-08 20:12:03 +0200

    create story model

---
+++ src/models/story.model.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const storySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  imagePath: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  excerpt: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  }
+})
+
+export default mongoose.model('Story', storySchema)
