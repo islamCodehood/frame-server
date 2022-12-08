f66160c6e77da0a13c423ff2276029404fabfe11
Islam Sayed <islam.sayed8@gmail.com>
2022-12-08 20:11:07 +0200

    add more user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { signin, signup, getAllUsers } from '../controllers/user.controllers'
+import { signin, signup, getAllUsers, getUser, followUser, removeUser, removeFollow, getFollowers, getFollowing } from '../controllers/user.controllers'
 
 const router = express.Router()
 
@@ -7,5 +7,10 @@ const router = express.Router()
 router.post('/signin', signin)
 router.post('/signup', signup)
 router.get('/all-users', getAllUsers)
-
+router.get('/:id', getUser)
+router.patch('/follow/:id', followUser)
+router.patch('/remove-follow/:id', removeFollow)
+router.delete('/remove-user/:id', removeUser)
+router.get('/:id/followers', getFollowers)
+router.get('/:id/following', getFollowing)
 export default router
