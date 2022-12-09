dda7105c1aadd11d052826c1b17325813a175534
Islam Sayed <islam.sayed8@gmail.com>
2022-12-09 13:34:04 +0200

    ad url to the model

--- src/models/story.model.ts
+++ src/models/story.model.ts
@@ -25,6 +25,10 @@ const storySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  url: {
+    type: String,
+    required: true,
+  }
 })
 
 export default mongoose.model('Story', storySchema)
