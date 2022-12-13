d4cfee069dd1121dceb15f9f98f0083657be93b5
Islam Sayed <islam.sayed8@gmail.com>
2022-12-14 00:03:22 +0200

    start new routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -10,6 +10,9 @@ import {
   getFollowers,
   getFollowing,
   addReview,
+  createList,
+  addToWantToWatch,
+  addToWatchedMovies
 } from '../controllers/user.controllers'
 
 const router = express.Router()
@@ -19,10 +22,13 @@ router.post('/signin', signin)
 router.post('/signup', signup)
 router.get('/all-users', getAllUsers)
 router.get('/:id', getUser)
-router.patch('/follow/:id', followUser)
-router.patch('/remove-follow/:id', removeFollow)
+router.post('/follow/:id', followUser)
+router.post('/remove-follow/:id', removeFollow)
 router.delete('/remove-user/:id', removeUser)
 router.get('/:id/followers', getFollowers)
 router.get('/:id/following', getFollowing)
 router.post('/:id/review', addReview)
+router.post('/:id/lists', createList)
+router.post('/:id/want-to-watch', addToWantToWatch)
+router.post('/:id/watched', addToWatchedMovies)
 export default router
