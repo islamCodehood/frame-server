7ecb53cd39324a88f0c4b17a2b2e65c5fb7597c4
Islam Sayed <islam.sayed8@gmail.com>
2022-12-14 00:02:54 +0200

    create movie model

---
+++ src/models/movie.model.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  postURL: {
+    type: String,
+    required: true,
+  },
+  id: { type: String, required: true },
+  overview: { type: String, required: true},
+  releaseDate: { type: String, required: true},
+  genres: Array,
+  reviews: Array,
+  rating: String,
+  wantToWatchUsers: Array,
+  watchedUsers: Array,
+})
+
+export default mongoose.model('Story', movieSchema)
